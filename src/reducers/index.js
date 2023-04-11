f3b49e3cb72c15ef47155fd2d6258f4ee6c29ab4
waqarabbas72 <sheraliate751@gmail.com>
2023-04-11 20:35:43 +0500

    song-selector

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+const songReducers = () => {
+    return [
+        {title : "Shubh No-Love" , duration : "3:15"},
+        {title : "Agency" , duration : "4:05"},
+        {title : "Gangster" , duration : "2:30"},
+        {title : "Serena-Safari" , duration : "5:00"}
+    ];
+};
+
+export default songReducers;
